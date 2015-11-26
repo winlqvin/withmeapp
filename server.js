0d9c013bb440f996ae264eea14256dca904093a6
Lê Quốc Vin <winlqvin.it@gmail.com>
2015-11-27 03:50:31 +0700

    Fix port

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ var express = require('express'),
     morgan = require('morgan');
 
 
-var DEFAULT_PORT =  process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var DEFAULT_PORT =  process.env.OPENSHIFT_NODEJS_PORT || 9000;
 var DEFAULT_HOST = process.env.OPENSHIFT_NODEJS_IP || 'localhost';
 
-if (DEFAULT_HOST == 'localhost'){
-    DEFAULT_PORT = 9000;
+if (DEFAULT_HOST != 'localhost'){
+    DEFAULT_PORT = 8080;
 }
 
 var app = express();
@@ -21,7 +21,7 @@ app.get('/', function(req, res){
 });
 
 
-
-
-
-app.listen(DEFAULT_PORT, DEFAULT_HOST);
+app.listen(DEFAULT_PORT, DEFAULT_HOST, function() {
+            console.log('%s: Node server started on %s:%d ...',
+                        Date(Date.now() ), DEFAULT_HOST, DEFAULT_PORT);
+        });
